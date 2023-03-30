8cee9cfc51ecaf9d983540046b47b5cbc7b1d443
Josh <josh@gable.tech>
2023-03-30 13:50:48 -0500

    modal popup delay

--- assets/script/script.js
+++ assets/script/script.js
@@ -50,98 +50,6 @@ const deleteCookie = (name) => {
   document.cookie = name + "=; Max-Age=-99999;";
 };
 
-/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-  Body
-
-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-
-/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
-
-  Popup Modal
-
-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
-
-// Accept Cookies
-
-function displayModal() {
-  if (
-    getCookie("browser") ||
-    getCookie("operatingSystem") ||
-    getCookie("ScreenWidth") ||
-    getCookie("ScreenHeight") ||
-    getCookie("enabled")
-  ) {
-    console.log("Cookies are present");
-  displayCookies();
-  } else {
-    console.log("no cookies");
-    modal.classList.remove("hide");
-  }
-}
-displayModal();
-
-function acceptCookies() {
-  setCookie("browser", getBrowser(), 15);
-  setCookie("operatingSystem", getOS(), 15);
-  setCookie("ScreenWidth", window.screen.availWidth, 15);
-  setCookie("ScreenHeight", window.screen.availHeight, 15);
-  displayCookies();
-  modal.classList.add("hide");
-}
-
-function openSettings() {
-  modal.classList.add("hide");
-  modalSettings.classList.remove("hide");
-}
-
-function savePref() {
-  modalSettings.classList.add("hide");
-  enabledCookies();
-  displayCookies();
-}
-
-function displayCookies() {
-  if(getCookie('browser') !== null) {
-    console.log(`Browser: ${getCookie('browser')}`);
-  }
-  if(getCookie('operatingSystem') !== null) {
-    console.log(`OS: ${getCookie('operatingSystem')}`);
-  }
-  if(getCookie('ScreenWidth') !== null) {
-    console.log(`SW: ${getCookie('ScreenWidth')}`);
-  }
-  if(getCookie('ScreenHeight') !== null) {
-    console.log(`SH: ${getCookie('ScreenHeight')}`);
-  }
-  if(getCookie('enabled') !== null) {
-    console.log(`Enabled: ${getCookie('enabled')}`);
-  }
-}
-
-function enabledCookies() {
-  toggleBrowser.checked
-    ? setCookie("browser", getBrowser(), 15)
-    : deleteCookie("browser");
-  toggleOS.checked
-    ? setCookie("operatingSystem", getOS(), 15)
-    : deleteCookie("operatingSystem");
-  toggleSW.checked
-    ? setCookie("ScreenWidth", window.screen.availWidth, 15)
-    : deleteCookie("ScreenWidth");
-  toggleSH.checked
-    ? setCookie("ScreenHeight", window.screen.availHeight, 15)
-    : deleteCookie("ScreenHeight");
-  if (
-    !toggleBrowser.checked &&
-    !toggleOS.checked &&
-    !toggleSW.checked &&
-    !toggleSH.checked
-  ) {
-    setCookie("enabled", "false", 15);
-  }
-}
-
 function getBrowser() {
   if (
     (navigator.userAgent.indexOf("Opera") ||
@@ -201,3 +109,97 @@ function getOS() {
   }
   return OSName;
 }
+
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+  Popup Modal
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+function displayModal() {
+  if (
+    getCookie("browser") ||
+    getCookie("operatingSystem") ||
+    getCookie("ScreenWidth") ||
+    getCookie("ScreenHeight") ||
+    getCookie("enabled")
+  ) {
+    console.log("Cookies are present");
+  displayCookies();
+  } else {
+    console.log("no cookies");
+    modal.classList.remove("hide");
+  }
+}
+setTimeout(() => {
+  displayModal();
+}, 1500);
+
+function acceptCookies() {
+  setCookie("browser", getBrowser(), 15);
+  setCookie("operatingSystem", getOS(), 15);
+  setCookie("ScreenWidth", window.screen.availWidth, 15);
+  setCookie("ScreenHeight", window.screen.availHeight, 15);
+  displayCookies();
+  modal.classList.add("hide");
+}
+
+function openSettings() {
+  modal.classList.add("hide");
+  modalSettings.classList.remove("hide");
+}
+
+function savePref() {
+  modalSettings.classList.add("hide");
+  enabledCookies();
+  displayCookies();
+}
+
+/*- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+  Functions
+
+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
+
+function displayCookies() {
+  if(getCookie('browser') !== null) {
+    console.log(`Browser: ${getCookie('browser')}`);
+  }
+  if(getCookie('operatingSystem') !== null) {
+    console.log(`OS: ${getCookie('operatingSystem')}`);
+  }
+  if(getCookie('ScreenWidth') !== null) {
+    console.log(`SW: ${getCookie('ScreenWidth')}`);
+  }
+  if(getCookie('ScreenHeight') !== null) {
+    console.log(`SH: ${getCookie('ScreenHeight')}`);
+  }
+  if(getCookie('enabled') !== null) {
+    console.log(`Enabled: ${getCookie('enabled')}`);
+  }
+}
+
+function enabledCookies() {
+  toggleBrowser.checked
+    ? setCookie("browser", getBrowser(), 15)
+    : deleteCookie("browser");
+  toggleOS.checked
+    ? setCookie("operatingSystem", getOS(), 15)
+    : deleteCookie("operatingSystem");
+  toggleSW.checked
+    ? setCookie("ScreenWidth", window.screen.availWidth, 15)
+    : deleteCookie("ScreenWidth");
+  toggleSH.checked
+    ? setCookie("ScreenHeight", window.screen.availHeight, 15)
+    : deleteCookie("ScreenHeight");
+  if (
+    !toggleBrowser.checked &&
+    !toggleOS.checked &&
+    !toggleSW.checked &&
+    !toggleSH.checked
+  ) {
+    setCookie("enabled", "false", 15);
+  }
+}
+
+
